b73b004e8121bdf9a94aa428e1d00be2bddad2fe
iil1078 <19599497+iil1078@users.noreply.github.com>
2023-11-12 22:58:30 -0600

    Create currency.py

---
+++ currency_converter/code/currency.py
@@ -0,0 +1,280 @@
+"""
+Module for currency exchange
+
+This module provides several string parsing functions to implement a simple
+currency exchange routine using an online currency service. The primary function
+in this module is exchange().
+
+Author: I-Chia Liao
+Date: 2023-08-03
+"""
+
+
+import introcs
+
+
+APIKEY = 'lOJyD4BjeDQn0aTAXxR5uuHaAxGeKvEt9Q3SK7YsTX4e'
+
+
+def before_space(s):
+    """
+    Returns the substring of s up to, but not including, the first space.
+
+    Example: before_space('Hello World') returns 'Hello'
+
+    Parameter s: the string to slice
+    Precondition: s is a string with at least one space in it
+    """
+
+    # check preconditions
+    assert type(s) == str, "type is not string"
+    assert introcs.find_str(s," ") != -1, "no space in the string"
+
+    # get result
+    first_space = introcs.find_str(s," ")
+    result = s[:first_space]
+    return result
+
+
+def after_space(s):
+    """
+    Returns the substring of s after the first space
+
+    Example: after_space('Hello World') returns 'World'
+
+    Parameter s: the string to slice
+    Precondition: s is a string with at least one space in it
+    """
+    
+    # check preconditions
+    assert type(s) == str, "type is not string"
+    assert introcs.find_str(s," ") != -1, "no space in the string"
+
+    # get result
+    first_space = introcs.find_str(s," ")
+    result = s[first_space+1:]
+    return result
+
+
+def first_inside_quotes(s):
+    """
+    Returns the first substring of s between two (double) quote characters
+
+    Note that the double quotes must be part of the string.  So "Hello World" is a 
+    precondition violation, since there are no double quotes inside the string.
+
+    Example: first_inside_quotes('A "B C" D') returns 'B C'
+    Example: first_inside_quotes('A "B C" D "E F" G') returns 'B C', because it only 
+    picks the first such substring.
+
+    Parameter s: a string to search
+    Precondition: s is a string with at least two (double) quote characters inside
+    """
+    # check preconditions
+    assert type(s) == str, "type is not string"
+    assert introcs.count_str(s,'"') >= 2, "need at least 2 double quotes"
+
+    # get result
+    start = introcs.find_str(s,'"')
+    end = introcs.find_str(s,'"',start+1)
+    result = s[start+1:end]
+    return result
+
+
+def get_src(json):
+    """
+    Returns the src value in the response to a currency query.
+
+    Given a JSON string provided by the web service, this function returns the string
+    inside string quotes (") immediately following the substring '"src"'. For example,
+    if the json is
+        
+        '{"success": true, "src": "2 United States Dollars", "dst": "1.772814 Euros", "error": ""}'
+
+    then this function returns '2 United States Dollars' (not '"2 United States Dollars"'). 
+    On the other hand if the json is 
+        
+        '{"success":false,"src":"","dst":"","error":"Source currency code is invalid."}'
+
+    then this function returns the empty string.
+
+    The web server does NOT specify the number of spaces after the colons. The JSON
+        
+        '{"success":true, "src":"2 United States Dollars", "dst":"1.772814 Euros", "error":""}'
+        
+    is also valid (in addition to the examples above).
+
+    Parameter json: a json string to parse
+    Precondition: json a string provided by the web service (ONLY enforce the type)
+    """
+
+    # check preconditions
+    assert type(json) == str, "type is not string"
+
+    # get result
+    index_src_end = introcs.find_str(json,'"src":') + 5
+    result = first_inside_quotes(json[index_src_end + 1:])
+    return result
+
+
+def get_dst(json):
+    """
+    Returns the dst value in the response to a currency query.
+
+    Given a JSON string provided by the web service, this function returns the string
+    inside string quotes (") immediately following the substring '"dst"'. For example,
+    if the json is
+        
+        '{"success": true, "src": "2 United States Dollars", "dst": "1.772814 Euros", "error": ""}'
+
+    then this function returns '1.772814 Euros' (not '"1.772814 Euros"'). On the other
+    hand if the json is 
+        
+        '{"success":false,"src":"","dst":"","error":"Source currency code is invalid."}'
+
+    then this function returns the empty string.
+
+    The web server does NOT specify the number of spaces after the colons. The JSON
+        
+        '{"success":true, "src":"2 United States Dollars", "dst":"1.772814 Euros", "error":""}'
+        
+    is also valid (in addition to the examples above).
+
+    Parameter json: a json string to parse
+    Precondition: json a string provided by the web service (ONLY enforce the type)
+    """
+    # check preconditions
+    assert type(json) == str, "type is not string"
+
+    # get result
+    index_dst_end = introcs.find_str(json,'"dst":') + 5
+    result = first_inside_quotes(json[index_dst_end + 1:])
+    return result
+
+
+def has_error(json):
+    """
+    Returns True if the response to a currency query encountered an error.
+
+    Given a JSON string provided by the web service, this function returns True if the
+    query failed and there is an error message. For example, if the json is
+        
+        '{"success":false,"src":"","dst":"","error":"Source currency code is invalid."}'
+
+    then this function returns True (It does NOT return the error message 
+    'Source currency code is invalid'). On the other hand if the json is 
+        
+        '{"success": true, "src": "2 United States Dollars", "dst": "1.772814 Euros", "error": ""}'
+
+    then this function returns False.
+
+    The web server does NOT specify the number of spaces after the colons. The JSON
+        
+        '{"success":true, "src":"2 United States Dollars", "dst":"1.772814 Euros", "error":""}'
+        
+    is also valid (in addition to the examples above).
+
+    Parameter json: a json string to parse
+    Precondition: json a string provided by the web service (ONLY enforce the type)
+    """
+    # check preconditions
+    assert type(json) == str, "type is not string"
+
+    # get result
+    index_error_end = introcs.find_str(json,'"error":') + 7
+    result = first_inside_quotes(json[index_error_end + 1:]) != ""
+    return result
+
+
+def service_response(src,dst,amt):
+    """
+    Returns a JSON string that is a response to a currency query.
+
+    A currency query converts amt money in currency src to the currency dst. The response 
+    should be a string of the form
+
+        '{"success": true, "src": "<src-amount>", dst: "<dst-amount>", error: ""}'
+
+    where the values src-amount and dst-amount contain the value and name for the src 
+    and dst currencies, respectively. If the query is invalid, both src-amount and 
+    dst-amount will be empty, and the error message will not be empty.
+
+    There may or may not be spaces after the colon.  To test this function, you should
+    chose specific examples from your web browser.
+
+    Parameter src: the currency on hand
+    Precondition src is a nonempty string with only letters
+
+    Parameter dst: the currency to convert to
+    Precondition dst is a nonempty string with only letters
+
+    Parameter amt: amount of currency to convert
+    Precondition amt is a float or int
+    """
+
+    # Check assertions
+    assert introcs.isalpha(src) == True
+    assert introcs.isalpha(dst) == True
+    assert (type(amt) == int or type(amt) == float), 'type is not int or float'
+
+    currency1 = src
+    currency2 = dst
+    amount = str(amt)
+    
+    q = ('https://ecpyfac.ecornell.com/python/currency/fixed?src=' + currency1 +
+    '&dst=' + currency2 + '&amt=' + amount + '&key=' + APIKEY)
+
+    result = introcs.urlread(q)
+    return result
+
+
+def iscurrency(currency):
+    """
+    Returns True if currency is a valid (3 letter code for a) currency.
+
+    It returns False otherwise.
+
+    Parameter currency: the currency code to verify
+    Precondition: currency is a nonempty string with only letters
+    """
+
+    assert type(currency) == str
+
+    json = service_response(currency,'TWD',2.5)
+    result = has_error(json) != True
+    return result
+    
+
+def exchange(src,dst,amt):
+    """
+    Returns the amount of currency received in the given exchange.
+
+    In this exchange, the user is changing amt money in currency src to the currency 
+    dst. The value returned represents the amount in currency currency_to.
+
+    The value returned has type float.
+
+    Parameter src: the currency on hand
+    Precondition src is a string for a valid currency code
+
+    Parameter dst: the currency to convert to
+    Precondition dst is a string for a valid currency code
+
+    Parameter amt: amount of currency to convert
+    Precondition amt is a float or int
+    """
+
+    # Check assertions
+    assert introcs.isalpha(src) == True
+    assert introcs.isalpha(dst) == True
+    assert (type(amt) == int or type(amt) == float), 'type is not int or float'
+
+    # Calc
+    # 1. Connect to the web service and get the json message
+    s = service_response(src,dst,amt)
+    # 2. Get result of dst
+    dst = get_dst(s)
+    # 3. Parse out the dst amount
+    dst_amt = float(before_space(dst))
+    # 4. Return dst amount
+    return dst_amt
